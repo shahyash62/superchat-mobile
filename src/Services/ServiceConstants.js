45a077c715def10c747fb9e4abb6b68fd83dfbc1
shahyash62 <shahyash62@gmail.com>
2020-08-25 21:44:27 -0400

    fixed base url issue

--- src/Services/ServiceConstants.js
+++ src/Services/ServiceConstants.js
@@ -1,5 +1,5 @@
-// export const baseURL = 'https://superchat62.herokuapp.com/';
-export const baseURL = 'http://localhost:5000/';
+export const baseURL = 'https://superchat62.herokuapp.com/';
+// export const baseURL = 'http://localhost:5000/';
 export const chatSocketURL = '/chat';
 export const loginURL = '/login';
 export const signupURL = '/signup';
